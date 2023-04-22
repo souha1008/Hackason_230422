14f9a12adc93f07d26a0d52df6a8bc2186e48bed
WTHS00975\Masaki <ka210100nin@gmail.com>
2023-04-22 14:36:27 +0900

    スコア関連6

--- Assets/Scripts/takino/ScoreManager.cs
+++ Assets/Scripts/takino/ScoreManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    [HideInInspector] public int Score;
     private void Awake()
     {
         DontDestroyOnLoad(this);
